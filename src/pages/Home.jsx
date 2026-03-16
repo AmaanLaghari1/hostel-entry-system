c1dc92e41411b9127fcb5b1d1105a4a4fe8431ad
Amaanullah Khan Lagari <89661480+AmaanLaghari1@users.noreply.github.com>
2026-03-16 11:54:52 +0500

    register route fixed, icon added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,27 +1,69 @@
 import Login from "./Login";
 import { useHostel } from "../context/HostelContext";
 import Register from "./Register";
-// import { useState } from "react";
 import Dashboard from "./Dashboard";
-import { HashRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Student from "./Student";
 import StudentLog from "./StudentLog";
 import AppLayout from "../components/AppLayout";
 
+import {
+  BrowserRouter,
+  HashRouter,
+  Routes,
+  Route,
+  Navigate
+} from "react-router-dom";
+
+// detect electron
+const isElectron = navigator.userAgent.toLowerCase().includes("electron");
+
+// choose router
+const Router = isElectron ? HashRouter : BrowserRouter;
+
 export default function Home() {
-  const { isLoggedIn } = useHostel()
-  // const [authSwitch, setAuthSwitch] = useState('login')
-    return (
-      <Router>
-        <Routes>
-          <Route path="/*" Component={() => <><h1>404 - Page Not Found</h1></> } />
-          <Route path="/abc/register" element={isLoggedIn ? <Navigate to="/" /> : <Register />} />
-          <Route path="/" element={isLoggedIn ? <AppLayout><Dashboard /></AppLayout> : <Login />} />
-          {/* <Route path="/" element={<AppLayout><WebcamCapture /></AppLayout>} /> */}
-          <Route path="/students" element={<AppLayout><Student /></AppLayout>} />
-          <Route path="/student_log" element={<AppLayout><StudentLog /></AppLayout>} />
-        </Routes>
-        
-      </Router>
-    );
+  const { isLoggedIn } = useHostel();
+
+  return (
+    <Router>
+      <Routes>
+        <Route path="/*" element={<h1>404 - Page Not Found</h1>} />
+
+        <Route
+          path="/"
+          element={
+            isLoggedIn ? (
+              <AppLayout>
+                <Dashboard />
+              </AppLayout>
+            ) : (
+              <Login />
+            )
+          }
+        />
+
+        <Route
+          path="/abc/register"
+          element={isLoggedIn ? <Navigate to="/" /> : <Register />}
+        />
+
+        <Route
+          path="/students"
+          element={
+            <AppLayout>
+              <Student />
+            </AppLayout>
+          }
+        />
+
+        <Route
+          path="/student_log"
+          element={
+            <AppLayout>
+              <StudentLog />
+            </AppLayout>
+          }
+        />
+      </Routes>
+    </Router>
+  );
 }
